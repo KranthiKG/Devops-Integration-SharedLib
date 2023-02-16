c0507426155ff98e4d2913e995be597dd82d3901
KranthiKG <97874149+KranthiKG@users.noreply.github.com>
2023-02-16 22:27:09 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to LTIMindtree";
+		return "Welcome To LTIMindtree";
 	}
 
 	public static void main(String[] args) {
